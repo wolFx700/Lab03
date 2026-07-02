5466859c35ac0cf849d71d58cce250e014858d25
kittipat chakan <168157736+wolFx700@users.noreply.github.com>
2026-07-02 16:58:59 +0700

    commit6

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
     echo "Hello Git HUB";
     echo "My name is Kittipat";
     echo "My nickname is Kittipat";
-    echo "6711130016"
-    echo "oraoraoraora"
+    echo "6711130016";
+    echo "oraoraoraora";
 ?>
